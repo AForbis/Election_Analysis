faa39bcaab41441d1dbd8eace5b011cdbe2bfd03
AlexF <alexforbis@utexas.edu>
2021-01-09 13:38:17 -0600

    Debugging outputs

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -48,7 +48,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-        county_name = row[2]
+        county_name = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -91,24 +91,36 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
+    for county_name in county_list:
 
         # 6b: Retrieve the county vote count.
+        votes = countyvotes_dict[county_name]
 
         # 6c: Calculate the percentage of votes for the county.
-
+        vote_percentage = float(votes) / float(total_votes) * 100
 
          # 6d: Print the county results to the terminal.
-
+        county_results = (f"{county_name}: {vote_percentage:.1f}% ({votes:,})\n")
+         
          # 6e: Save the county votes to a text file.
-
+        
          # 6f: Write an if statement to determine the winning county and get its vote count.
-
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_county = county_name
+            winning_percentage = vote_percentage
 
     # 7: Print the county with the largest turnout to the terminal.
-
+    winning_county_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_county}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(winning_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
